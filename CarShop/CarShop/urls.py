6bcc676ab4c662f5a2ddc62660a984f40200a1ce
Pavelabramovich <apn20040510@gmail.com>
2023-05-27 00:33:09 +0300

    add suprUser

--- CarShop/CarShop/urls.py
+++ CarShop/CarShop/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from test_app import views # vsie good na samom dele
+from django.urls import path, include
+from test_app import views     # vsie good na samom dele
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('admin/', admin.site.urls),
+    path('', include('test_app.urls'))
 ]
+
+#urlpatterns = [
+#    path('', views.index, name='home'),
+#]
