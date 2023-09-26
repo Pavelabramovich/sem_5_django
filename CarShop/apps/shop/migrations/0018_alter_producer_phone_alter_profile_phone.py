efa445f7180cd4b5d842c0b26be3f819251470a8
Pavelabramovich <apn20040510@gmail.com>
2023-09-26 22:38:44 +0300

    fix migrations order

--- CarShop/apps/shop/migrations/0018_alter_producer_phone_alter_profile_phone.py
+++ CarShop/apps/shop/migrations/0018_alter_producer_phone_alter_profile_phone.py
@@ -21,9 +21,4 @@ class Migration(migrations.Migration):
             name='phone',
             field=models.CharField(help_text='Enter a phone in format +375 (29) XXX-XX-XX', max_length=64, validators=[apps.shop.validators.FullMatchRegexValidator('\\+375\\s*\\(\\s*29\\s*\\)\\s*(\\d{3})\\s*-\\s*(\\d{2})\\s*-\\s*(\\d{2})', 'Phone number is incorrect. Correct format is +375 (29) XXX-XX-XX.', code='invalid')]),
         ),
-        migrations.AlterField(
-            model_name='provider',
-            name='phone',
-            field=models.CharField(help_text='Enter a phone in format +375 (29) XXX-XX-XX', max_length=64, validators=[apps.shop.validators.FullMatchRegexValidator('\\+375\\s*\\(\\s*29\\s*\\)\\s*(\\d{3})\\s*-\\s*(\\d{2})\\s*-\\s*(\\d{2})', 'Phone number is incorrect. Correct format is +375 (29) XXX-XX-XX.', code='invalid')]),
-        ),
     ]
